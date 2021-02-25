1aed98d89bbc7a70f55e1143d66073d80c380114
Mojtaba Raouf <m.raouf@ipm.ir>
2021-02-25 17:08:36 +0900

    lock

--- SIMBA_get_gals.py
+++ SIMBA_get_gals.py
@@ -1,1 +1,3 @@
-Estimate the ram pressure striping using SIMBA and CAESAR OUTPUT (private)
+version https://git-lfs.github.com/spec/v1
+oid sha256:25673b11f9a13ad89fbb60d8590ee1fd6cc9071f1356b5ae57f007dec29f4cff
+size 74
