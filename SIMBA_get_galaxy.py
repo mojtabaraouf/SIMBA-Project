b9e8c9bb89481ac9ad3c0a7798259a1d2538da0a
Mojtaba Raouf <m.raouf@ipm.ir>
2021-02-25 18:29:17 +0900

    new

---
+++ SIMBA_get_galaxy.py
@@ -0,0 +1,13 @@
+#Estimate the kinematic position angle for gas, stars and dark matter at 2Re Illustris-TNG simualtion
+#----------------------------------------------------------------------------------------------------
+#--------------------------------------------mojtabaraouf@gmail.com----------------------------------
+#SIMBA project-----------------------------------------------------------------------------------------
+## Release soon(do not hesitate to send me an email----------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------
+
+import h5py
+import numpy as np
+import os
+import caesar
+from astropy import constants as const
+from readgadget import *
